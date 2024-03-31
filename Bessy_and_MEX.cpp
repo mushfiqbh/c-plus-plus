aa619f8f9f1246ba5318e0d300a333b531532adb
mushfiqbh <mushfiqbh@gmail.com>
2024-03-31 17:11:08 +0600

    update

--- Bessy_and_MEX.cpp
+++ Bessy_and_MEX.cpp
@@ -1,50 +1,53 @@
 // Copyright: Mushfiq R.
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-int mex(vector<int> arr);
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t;
-    cin >> t;
-	while (t--)
-	{
-        int n;
-        cin >> n;
-        int a[n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    {
+        long long t;
+        cin >> t;
+
+        while (t--)
+        {
+            long long n;
+            cin >> n;
+
+            vector<long long> arr(n);
+            for (long long i = 0; i < n; i++)
+            {
+                cin >> arr[i];
+            }
+
+            vector<long long> prr(n);
+            long long cmex = n;
+
+            for (long long i = n - 1; i >= 0; i--)
+            {
+                if (arr[i] > 0)
+                {
+                    int mex = cmex - arr[i];
+                    prr[i] = mex;
+                    cmex = mex;
+                }
+                else
+                {
+                    prr[i] = cmex + abs(arr[i]);
+                }
+            }
+
+            for (long long i = 0; i < n; i++)
+            {
+                cout << prr[i] << " ";
+            }
+            cout << endl;
         }
-	}
 
-	return 0;
-}
-
-
-int mex(vector<int> arr) {
-    N = arr.length();
-    int freq[N + 1];
-    for (int i = 0; i <= N; i++) {
-        freq[i] = 0;
+        return 0;
     }
-
-    for (int i = 0; i < N; i++) {
-        if (arr[i] >= 0 && arr[i] <= N) {
-            freq[arr[i]]++;
-        }
-    }
-
-    int mex = 0;
-    for (int i = 0; i <= N; i++) {
-        if (freq[i] == 0) {
-            mex = i;
-            break;
-        }
-    }
-
-    return mex;
 }
