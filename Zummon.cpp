59eec89f3e9a70c0374a6b603091e2732de4b52c
mushfiqbh <mushfiqbh@gmail.com>
2024-04-06 20:40:36 +0600

    OK

--- Zummon.cpp
+++ Zummon.cpp
@@ -2,42 +2,17 @@
 #include <iostream>
 using namespace std;
 
-#include <stdio.h>
-
-// Function to find the MEX of an array
-int mex(int arr[], int N)
-{
-	int freq[N + 1];
-	for (int i = 0; i <= N; i++)
-	{
-		freq[i] = 0;
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		if (arr[i] >= 0 && arr[i] <= N)
-		{
-			freq[arr[i]]++;
-		}
-	}
-
-	int mex = 0;
-	for (int i = 0; i <= N; i++)
-	{
-		if (freq[i] == 0)
-		{
-			mex = i;
-			break;
-		}
-	}
-
-	return mex;
-}
-
 int main()
 {
-	int arr[] = {1, 0, 4};
-	int N = sizeof(arr) / sizeof(arr[0]);
-	printf("MEX of the array: %d\n", mex(arr, N));
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+    cin >> t;
+	while (t--)
+	{
+        
+	}
+
 	return 0;
 }
