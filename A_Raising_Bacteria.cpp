919c724f9c7246ddfbd98832301448d5eb302d07
mushfiqbh <mushfiqbh@gmail.com>
2024-05-02 20:19:13 +0600

    OK

---
+++ A_Raising_Bacteria.cpp
@@ -0,0 +1,17 @@
+// Copyright: Mushfiq R.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+    cin >> n;
+    while(true){
+
+    }
+
+	return 0;
+}
