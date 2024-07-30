df809d369a129180e3c69bbc23889d9ad1a37e1d
mushfiqbh <mushfiqbh@gmail.com>
2024-07-30 15:46:11 +0600

    Update

---
+++ __Template.cpp
@@ -0,0 +1,35 @@
+// Problem Solver: Mushfiq R.
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+#define NIL -1
+#define MAX 1000
+typedef long long ll;
+int lookup[MAX];
+
+void lookup_init()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        lookup[i] = NIL;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        
+    }
+
+    return 0;
+}
+
+
