3a1ef1546c4e128d08ee135b0385720b5d891c6c
mushfiqbh <mushfiqbh@gmail.com>
2024-10-19 20:09:07 +0600

    OK

--- DSA/Singly-Linked-List.cpp
+++ DSA/Singly-Linked-List.cpp
@@ -32,15 +32,6 @@ struct SLL
     void print();
 };
 
-/**
- * Inserts a new node at the beginning of the list.
- *
- * If the list is empty, head is set to the new node.
- * Otherwise, the new node is inserted before the current head node and the
- * head pointer is updated to point to the new node.
- *
- * @param value The data to be stored in the new node.
- */
 void SLL::insertHead(int value)
 {
     Node *newNode = new Node(value);
@@ -56,16 +47,6 @@ void SLL::insertHead(int value)
     }
 }
 
-/**
- * Inserts a new node at the end of the list.
- *
- * If the list is empty, head is set to the new node.
- * Otherwise, the new node is appended to the end of the list
- * by traversing the list until the last node is found and its
- * next pointer is set to the new node.
- *
- * @param value The data to be stored in the new node.
- */
 void SLL::insertEnd(int value)
 {
     Node *newNode = new Node(value);
@@ -85,17 +66,6 @@ void SLL::insertEnd(int value)
     }
 }
 
-/**
- * Inserts a new node at the given position in the list.
- *
- * If the position is 0, the new node is inserted at the beginning of the list.
- * If the position is greater than the length of the list, the new node is not
- * inserted. Otherwise, the new node is inserted after the node at the given
- * position in the list.
- *
- * @param value The data to be stored in the new node.
- * @param pos The position in the list where the new node is to be inserted.
- */
 void SLL::insertAtPos(int value, int pos)
 {
     if (pos < 0)
@@ -109,7 +79,8 @@ void SLL::insertAtPos(int value, int pos)
     {
         head = newNode;
     }
-    else    {
+    else
+    {
         Node *current = head;
         int count = 0;
 
@@ -130,12 +101,6 @@ void SLL::insertAtPos(int value, int pos)
     }
 }
 
-/**
- * Prints the contents of the list.
- *
- * If the list is empty, prints a message.
- * Otherwise, prints the data of each node in the list, separated by a space.
- */
 void SLL::print()
 {
     if (head == NULL)
@@ -154,13 +119,6 @@ void SLL::print()
     }
 }
 
-/**
- * Deletes the first node in the list.
- *
- * If the list is empty, prints a message.
- * Otherwise, deletes the node at the head of the list and updates the head
- * pointer to point to the next node in the list.
- */
 void SLL::deleteHead()
 {
     if (head == NULL)
@@ -175,14 +133,6 @@ void SLL::deleteHead()
     }
 }
 
-/**
- * Deletes the last node in the list.
- *
- * If the list is empty, prints a message.
- * If the list has one node, deletes the node and sets the head to NULL.
- * Otherwise, traverses the list to find the second-to-last node, deletes its
- * next node, and sets its next pointer to NULL.
- */
 void SLL::deleteTail()
 {
     if (head == NULL)
@@ -208,17 +158,6 @@ void SLL::deleteTail()
     }
 }
 
-/**
- * Deletes the node at the given position in the list.
- *
- * If the list is empty, prints a message.
- * If the position is 1, deletes the first node and sets the head to its next node.
- * Otherwise, traverses the list to find the node at the given position, deletes
- * it, and sets the next pointer of the previous node to the node after the
- * deleted node.
- *
- * @param pos The position of the node to delete in the list.
- */
 void SLL::deleteAtPos(int pos)
 {
     if (head == NULL)
@@ -244,15 +183,6 @@ void SLL::deleteAtPos(int pos)
     }
 }
 
-/**
- * Searches the list for the given item.
- *
- * Traverses the list until the given item is found or the end of the list is
- * reached. If the item is found, prints a message and returns. Otherwise, prints
- * a different message.
- *
- * @param item The item to search for in the list.
- */
 void SLL::searchItem(int item)
 {
     Node *h = head;
@@ -277,9 +207,7 @@ int main()
     li.insertHead(5);
 
     li.print();
-    li.deleteHead();
-    li.print();
-    li.deleteTail();
+    li.deleteAtPos(1);
     li.print();
 
     return 0;
